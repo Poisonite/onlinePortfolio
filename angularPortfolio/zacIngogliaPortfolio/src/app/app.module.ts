f37d413d7e7a025a069a3a7d13f8447ac8ed83f2
Zac <zacace@ingoglia.com>
2020-08-18 15:31:56 -0700

    08-18-2020 Update 1
    +Imported basic Material pieces
    +Built out nav and top bar
    
    App.component is pretty much ready, need to set up other pages now

--- angularPortfolio/zacIngogliaPortfolio/src/app/app.module.ts
+++ angularPortfolio/zacIngogliaPortfolio/src/app/app.module.ts
@@ -1,14 +1,23 @@
+// Core+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { AboutComponent } from './components/about/about.component';
 import { ReferencesComponent } from './components/references/references.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,14 +26,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ResumeComponent,
     SkillsComponent,
     AboutComponent,
-    ReferencesComponent
+    ReferencesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
