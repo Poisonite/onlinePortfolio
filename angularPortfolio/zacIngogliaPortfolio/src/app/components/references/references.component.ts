f1dacea2e61a0a84541992f0f3124459dfdca8a7
Zac <zacace@ingoglia.com>
2020-08-21 22:21:54 -0700

    08-20&21-2020 Update 1
    +Added and configured custom theme settings across site
    *Updated components to use Angular Material cards
    *Properly flex box styled cards

--- angularPortfolio/zacIngogliaPortfolio/src/app/components/references/references.component.ts
+++ angularPortfolio/zacIngogliaPortfolio/src/app/components/references/references.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-references',
   templateUrl: './references.component.html',
-  styleUrls: ['./references.component.scss']
+  styleUrls: ['./references.component.scss'],
 })
 export class ReferencesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // Function to handle sending reference request
+  contactReference(name) {
+    console.log(`Clicked ${name}`);
   }
-
 }
